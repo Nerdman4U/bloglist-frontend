f80ab570d176eec4ead29f7313fdd0e8d3eca140
Joni Toyryla <info@jonitoyryla.eu>
2024-01-24 15:55:24 +0200

    Tehtävä 5.10

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,17 +1,37 @@
 import BlogLine from "./BlogLine"
 import Togglable from "./Togglable"
+import { useState, useEffect } from 'react'
 
 const Blog = ({ blogItems, setNoteMessage }) => {
+  const [ sortedItems, setSortedItems ] = useState(blogItems)
+
+  const compareNumbers = (a, b) => a-b
+
+  const sort = () => {
+    const sorted = blogItems.sort((a, b) => compareNumbers(b.likes, a.likes))
+    setSortedItems(sorted)
+  }
+
+  const update = (id, likes) => {
+    const item = blogItems.find(blogItem => blogItem.id === id)
+    item.likes = likes
+    sort()
+  }
+
+  useEffect(() => {
+    sort()
+  })
+
   return (
     <div>
       <div>
-      { blogItems.map(
+      { sortedItems.map(
           blogItem => {
             return (
               <div key={blogItem.id} style={{border:"1px solid black", margin:"5px 0", padding:"10px"}}>
                 <div>Title: {blogItem.title}</div>
                 <Togglable labels={{open: 'View', close: 'Hide'}}>
-                  <BlogLine key={blogItem.id} blog={blogItem} setNoteMessage={setNoteMessage} />
+                  <BlogLine key={blogItem.id} blog={blogItem} setNoteMessage={setNoteMessage} update={update}/>
                 </Togglable>
               </div>
             )
--- src/components/BlogLine.jsx
+++ src/components/BlogLine.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import blogService from '../services/blogs'
-const BlogLine = ({ blog, setNoteMessage }) => {
+const BlogLine = ({ blog, setNoteMessage, update }) => {
   const [likes, setLikes] = useState(blog.likes)
 
   const likeBlogHandler = (blog) => {
-    console.log('likeBlogHandler()', blog)
+    //console.log('likeBlogHandler()', blog)
     const newLikes = likes + 1
+    //console.log('New likes', newLikes)
     setLikes(newLikes)
-    blogService.update(blog.id, {'title': blog.title, 'author': blog.author, 'url': blog.url, 'likes': likes})
-    setNoteMessage(`Likes ${likes}`)
+    blogService.update(blog.id, {'title': blog.title, 'author': blog.author, 'url': blog.url, 'likes': newLikes})
+    setNoteMessage(`Likes ${newLikes}`)
     setTimeout(() => {
       setNoteMessage(null)
     }, 3000)
+    update(blog.id, newLikes)
   }
 
   return (

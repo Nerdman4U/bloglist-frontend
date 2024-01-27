41368706092fb10dd79f99e67f841804cae93d08
Joni Toyryla <info@jonitoyryla.eu>
2024-01-27 13:22:08 +0200

    Tehtävä 5.14

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -2,22 +2,40 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
-test('renders content', () => {
-  const blog = {
-    title: 'my great new log title',
-    author: 'me',
-    likes: 1,
-    url: 'http://exampl.e.com'
-  }
+describe('Blog', () => {
+  let blog, removeBlog, increateLikes, setNoteMessage
+  beforeEach(() => {
+    blog = {
+      title: 'my great new log title',
+      author: 'me',
+      likes: 1,
+      url: 'http://exampl.e.com'
+    }
+    removeBlog = jest.fn()
+    increateLikes = jest.fn()
+    setNoteMessage = jest.fn()
+  })
 
-  const removeBlog = jest.fn()
-  const increateLikes = jest.fn()
-  const setNoteMessage = jest.fn()
+  test('renders content', () => {
+    render(<Blog blog={blog} removeBlog={removeBlog} increaseLikes={increateLikes} setNoteMessage={setNoteMessage} />)
+    const element = screen.getByText('Title: my great new log title')
+    expect(element).toBeDefined()
+  })
 
-  render(<Blog blog={blog} removeBlog={removeBlog} increaseLikes={increateLikes} setNoteMessage={setNoteMessage} />)
+  test('click how more button to view full details of blog post', async () => {
+    render(<Blog blog={blog} removeBlog={removeBlog} increaseLikes={increateLikes} setNoteMessage={setNoteMessage} />)
+    const user = userEvent.setup()
+    const button = screen.getByText('View')
+    expect(button).toBeDefined()
+    await user.click(button)
 
-  const element = screen.getByText('Title: my great new log title')
-  expect(element).toBeDefined()
+    expect(screen.getByText('Title: my great new log title') ).toBeDefined()
+    expect(screen.findByText('Author: me')).toBeDefined()
+    expect(screen.findByText('Likes: 1')).toBeDefined()
+    expect(screen.findByText('Url: http://exampl.e.com')).toBeDefined()
+  })
+
 })
 
